b6ef1096a9db733bb080141b8d4bf5e56aaf11af
Andy Armstrong <andy@hexten.net>
2021-11-13 19:14:35 +0000

    !text

--- bin/fix00-hex-to-string.js
+++ bin/fix00-hex-to-string.js
@@ -5,10 +5,48 @@ async function readSource(src) {
   return txt.split(/\n/);
 }
 
+function acmeString(bytes) {
+  const args = [];
+  let inString = false;
+  for (const b of bytes) {
+    if (b >= 32 && b < 127 && b !== 0x22) {
+      // printable, not a quote
+      if (args.length === 0 || typeof args[args.length - 1] !== "string")
+        args.push("");
+      args[args.length - 1] += String.fromCharCode(b);
+    } else {
+      args.push(b);
+    }
+  }
+  return args
+    .map((arg) =>
+      typeof arg === "string"
+        ? `"${arg}"`
+        : arg === 0
+        ? 0
+        : `\$${arg.toString(16)}`
+    )
+    .join(", ");
+}
+
+function fixHex(line) {
+  const m = line.match(/^(.*)!hex ([0-9a-f]{2}(?: [0-9a-f]{2})+)/);
+  if (!m) return line;
+  const [, prefix, hex] = m;
+  const bytes = hex.split(/ /).map((h) => parseInt(h, 16));
+  const rep = acmeString(bytes);
+  return `${prefix}!text ${rep}`;
+}
+
 async function main(args) {
   for (const src of args) {
     const lines = await readSource(src);
-    for (let l = 0; l < lines.length; l++) {}
+    for (let l = 0; l < lines.length; l++) {
+      const line = lines[l];
+      const nl = fixHex(line);
+      lines[l] = nl;
+    }
+    console.log(lines.join("\n"));
   }
 }
 
