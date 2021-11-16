8ae388853df9f670876154cca10381eea15928dd
Andy Armstrong <andy@hexten.net>
2021-11-16 15:26:50 +0000

    Rename addressing modes.

--- bin/mule.js
+++ bin/mule.js
@@ -3,22 +3,27 @@ require("../use");
 const syntax = require("ref/6502");
 
 const { MOS6502 } = require("lib/6502/cpu");
-const { MOS6502Assembler } = require("lib/6502/assembler");
+const { MOS6502Assembler, extractExpression } = require("lib/6502/assembler");
 
 // const cpu = new MOS6502(syntax);
 // console.log(MOS6502.statusC);
 
-const asm = new MOS6502Assembler(syntax);
-const src = [
-  `define("byte", (...bytes) => {`,
-  `  for (const b of bytes) putByte(b);`,
-  `});`,
-  `/// prstr         ldx #msg_len - 1         ; see above`,
-  `/// -             lda msg, x`,
-  `///               jsr oswrch`,
-  `///               dex`,
-  `///               bpl -`,
-  `///               rts`
-].join("\n");
+// const asm = new MOS6502Assembler(syntax);
+// const src = [
+//   `=js`,
+//   ``,
+//   `define("byte", (...bytes) => {`,
+//   `  for (const b of bytes) putByte(b);`,
+//   `});`,
+//   ``,
+//   `=asm`,
+//   ``,
+//   `prstr         ldx #msg_len - 1         ; see above`,
+//   `-             lda msg, x`,
+//   `              jsr oswrch`,
+//   `              dex`,
+//   `              bpl -`,
+//   `              rts`
+// ].join("\n");
 
-const code = asm.compile(src);
+// const code = asm.compile(src);
