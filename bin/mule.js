8fb552b828b484d17a63b96b4d89c3d6df5b6ddd
Andy Armstrong <andy@hexten.net>
2021-11-16 19:39:02 +0000

    compileExpression

--- bin/mule.js
+++ bin/mule.js
@@ -6,8 +6,7 @@ const { MOS6502 } = require("lib/6502/cpu");
 const {
   MOS6502Assembler,
   MOS6502Build,
-  compileExpression,
-  exParse
+  compileExpression
 } = require("lib/6502/assembler");
 
 // const cpu = new MOS6502(syntax);
@@ -19,16 +18,26 @@ const exs = [
   "(0)",
   "-0b111",
   "< 0xffee",
+  "> $ffee",
   "9 * $ff / 2 + .bee",
   ":A / 2 + foo",
-  "-- + 3",
+  "3 + -",
   "A + 1",
   "Math.floor(.x / 3)",
+  "Math.min(:rom, 0x800)",
   `"\\""`,
-  "`:x ${x} OK?`"
+  "`:x ${x}`",
+  "A + 2 | A - 3",
+  "!(A <= 3 && b !== $80)",
+  "A < 10 ? B / 2 : C & 0xFFDD",
+  "terminate('Hello')"
+  // "terminate('Hello')(1)"
+  // "foo({.name, ...rest})"
 ];
 
 for (const ex of exs) {
-  const comp = exParse(ex);
-  console.log(JSON.stringify(comp, null, 2));
+  const comp = compileExpression(ex);
+  console.log(`== ${ex} ==`);
+  // console.log(JSON.stringify(comp, null, 2));
+  console.log(`${ex} => ${comp.join("")}`);
 }
